9777fb92c18c1bb6086ac19c8ea951affd6db902
yangjie <hiyoungjay@gmail.com>
2016-01-05 10:38:22 +0800

    cascadefactoryconfig data constructor

--- src/main/java/com/dianping/cascade/CascadeFactoryConfig.java
+++ src/main/java/com/dianping/cascade/CascadeFactoryConfig.java
@@ -1,14 +1,18 @@
 package com.dianping.cascade;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Builder;
 
 /**
  * Created by yangjie on 1/3/16.
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CascadeFactoryConfig {
-    private int threadCount;
+    private int threadCount = 1;
 
     public final static CascadeFactoryConfig DEFAULT = new CascadeFactoryConfig();
 }
